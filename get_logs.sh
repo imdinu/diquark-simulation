e6d5829996aa7061f63712a0d6619bd888c61e30
Ioana Duminica <dinu.ioan@outlook.com>
2024-01-15 13:41:13 +0200

    minor fixes

--- get_logs.sh
+++ get_logs.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-mkdir logs
+LOG_DIR="LOGS_TEST"
+
+mkdir $LOG_DIR
 
 # Copy logs from all containers
 for container in $(docker ps -aq); do
-    docker logs $container > "logs/logs_$container.log"
+    CONTAINER_NAME=$(docker ps -a --filter "id=$container" | tail -n +2 | awk '{print $12}')
+    docker logs $container > "$LOG_DIR/logs_$CONTAINER_NAME.log"
 done
